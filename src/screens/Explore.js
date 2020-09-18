5530bfd4d50bbb5942bb2d27f58a96cfefc6d442
Aniketh Panchidi <ani.p1995@mail.com>
2020-09-19 01:23:24 +0530

    added view for searchbar

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useRef, useEffect} from 'react';
 import {
   Container,
   Header,
@@ -30,33 +30,80 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 import LinearGradient from 'react-native-linear-gradient';
 import * as Animatable from 'react-native-animatable';
 import {color} from 'react-native-reanimated';
-
+import Autocomplete from 'react-native-autocomplete-input';
 import ExploreCard from '../components/ExploreCard';
 
 const Explore = ({navigation}) => {
-  const pressHandler = () => {
-    navigation.navigate('Splashscreen');
-  };
+  // const pressHandler = () => {
+  //   navigation.navigate('Splashscreen');
+  // };
+
+  const [display, setDisplay] = useState(false);
+  // const [options, setOptions] = useState([]);
+  // const [search, setSearch] = useState('');
+  // const wrapperRef = useRef(null);
+
+  const data = [
+    {name: 'cotton'},
+    {name: 'Texurise'},
+    {name: 'PSF'},
+    {name: '60Cotton'},
+    {name: 'texurise'},
+  ];
+
+  // useEffect(() => {
+  //   window.addEventListener('mousedown', handleClickOutside);
+  //   return () => {
+  //     window.removeEventListener('mousedown', handleClickOutside);
+  //   };
+  // });
+
+  // const handleClickOutside = (event) => {
+  //   const {current: wrap} = wrapperRef;
+  //   if (wrap && !wrap.contains(event.target)) {
+  //     setDisplay(false);
+  //   }
+  // };
+
+  // setOptions(data);
 
   return (
     <View style={style.container}>
-      <Header
-        searchBar
-        rounded
-        style={{backgroundColor: 'white', flexDirection: 'row'}}>
-        <Item
-          style={{
-            borderRadius: 50,
-            backgroundColor: '#d3d3d3',
-            flex: 8,
-          }}>
-          <FeatherIcon name="search" size={25} style={{margin: 5}} />
-          <Input placeholder="Search" />
-        </Item>
-        <Item style={{justifyContent: 'center'}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: 'white',
+          alignItems: 'center',
+          height: 60,
+        }}>
+        <View style={{flex: 8, margin: 5, borderRadius: 50}}>
+          <TouchableOpacity
+            style={{
+              borderRadius: 50,
+              backgroundColor: '#d3d3d3',
+              flexDirection: 'row',
+              height: 40,
+            }}
+            onPress={() => {
+              setDisplay(!display);
+            }}>
+            <FeatherIcon name="search" size={25} style={{margin: 5}} />
+            <TextInput placeholder="Search" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <FontAwesomeIcon name="heart" size={25} />
-        </Item>
-      </Header>
+        </View>
+      </View>
+      {display && (
+        <View style={{height: 200, width: null, backgroundColor: 'white'}}>
+          <Text>hello</Text>
+          <Text>hello</Text>
+          <Text>hello</Text>
+          <Text>hello</Text>
+        </View>
+      )}
       <View>
         <View style={{margin: 10}}>
           <Text>Recents</Text>
